2115d97bc1fd6b34da82265779ba24765a048e8c
Tomas Sirio <tsirio@bol.com>
2022-06-07 10:01:59 +0200

    remove unused classes

--- src/main/java/com/rerollyourbody/gymbro/core/model/Set.java
+++ src/main/java/com/rerollyourbody/gymbro/core/model/Set.java
@@ -1,12 +1,14 @@
 package com.rerollyourbody.gymbro.core.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Set {
     private Integer repetitions;
     private Float weight;
